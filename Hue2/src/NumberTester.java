1094da7388dfda015e37dc707c393535067e10ec
Oberwagner <OberwagnerL22091@sus.htl-grieskirchen.at>
2024-09-24 12:16:39 +0200

    Created Setter Methods in NumberTester Class

--- Hue2/src/NumberTester.java
+++ Hue2/src/NumberTester.java
@@ -1,23 +1,27 @@
 
 public class NumberTester {
 
-    public NumberTester(String fileName){
+    NumberTest primeTester;
+    NumberTest oddTester;
+    NumberTest palindromeTester;
 
+    public NumberTester(String fileName){
+        //TODO
     }
 
     public void setOddEvenTester(NumberTest oddTester){
-
+        this.oddTester = oddTester;
     }
 
     public void setPrimeTester(NumberTest primeTester){
-
+        this.primeTester = primeTester;
     }
 
     public void setPalindromeTester(NumberTest palindromeTester){
-
+        this.palindromeTester = palindromeTester;
     }
 
     public void testFile(){
-
+        //TODO
     }
 }
