7a9cd1a5cdd6cfdbe00701f5845e56e6aae81fec
Oberwagner <OberwagnerL22091@sus.htl-grieskirchen.at>
2024-09-24 12:03:56 +0200

    Created Methods for NumberTester Class

--- Hue2/src/NumberTester.java
+++ Hue2/src/NumberTester.java
@@ -1,3 +1,23 @@
+
 public class NumberTester {
 
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester){
+
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester){
+
+    }
+
+    public void testFile(){
+        
+    }
 }
