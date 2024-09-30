d72a95af54b29bbf4755997817b6312ed8b3f46c
OberwagnerL <OberwagnerL22091@sus.htl-grieskirchen.at>
2024-09-30 17:31:17 +0200

    Abstract Class AbstractClaculator created

---
+++ Hue2/src/AbstractCalculator.java
@@ -0,0 +1,15 @@
+public abstract class AbstractCalculator {
+    protected CalculationOperation add;
+    protected CalculationOperation substract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculator(CalculationOperation add, CalculationOperation substract, CalculationOperation multiply, CalculationOperation divide) {
+        
+    }
+
+    public abstract Number add(Number a, Number b);
+    public abstract Number substract(Number a, Number b);
+    public abstract Number multiply(Number a, Number b);
+    public abstract Number divide(Number a, Number b);
+}
