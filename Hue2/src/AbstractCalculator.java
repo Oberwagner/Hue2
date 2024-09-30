5a8b7033ee98bb3a4fc61a245fa2ca0843adde65
OberwagnerL <OberwagnerL22091@sus.htl-grieskirchen.at>
2024-09-30 17:43:31 +0200

    AbstractCalculator + Number Classes changed

--- Hue2/src/AbstractCalculator.java
+++ Hue2/src/AbstractCalculator.java
@@ -1,15 +1,18 @@
 public abstract class AbstractCalculator {
     protected CalculationOperation add;
-    protected CalculationOperation substract;
+    protected CalculationOperation subtract;
     protected CalculationOperation multiply;
     protected CalculationOperation divide;
 
-    public AbstractCalculator(CalculationOperation add, CalculationOperation substract, CalculationOperation multiply, CalculationOperation divide) {
-        
+    public AbstractCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        this.add = add;
+        this.subtract = subtract;
+        this.multiply = multiply;
+        this.divide = divide;
     }
 
     public abstract Number add(Number a, Number b);
-    public abstract Number substract(Number a, Number b);
+    public abstract Number subtract(Number a, Number b);
     public abstract Number multiply(Number a, Number b);
     public abstract Number divide(Number a, Number b);
 }
--- Hue2/src/Number.java
+++ Hue2/src/Number.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Number {
 
     private double a;
     private double b;
+
+    public Number(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
 
     public double getA() {
         return a;
@@ -17,5 +24,10 @@ public class Number {
 
     public void setB(double b) {
         this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "a = " + a + ", b = " + b;
     }
 }
